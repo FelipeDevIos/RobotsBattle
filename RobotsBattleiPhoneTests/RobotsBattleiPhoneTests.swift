1f47f7a7a39a391a89a0edf72156b1612596cab6
Felipe Velandia <Felipe Velandia>
2022-11-27 10:23:26 -0500

    Adding test_prizeHasNoValidPosition and test_prizePositionIsOnBoard

--- RobotsBattleiPhoneTests/RobotsBattleiPhoneTests.swift
+++ RobotsBattleiPhoneTests/RobotsBattleiPhoneTests.swift
@@ -26,4 +26,31 @@ final class RobotsBattleiPhoneTests: XCTestCase {
         
         XCTAssertEqual(position, cell.toPosition(indexPathTest))
     }
+    
+    func test_prizeHasValidPosition() {
+        let prizePosition = Position(x: 4, y: 4)
+        var playedCells = [BattleCell]()
+        playedCells.append(BattleCell(position: Position(x: 1, y: 1)))
+        playedCells.append(BattleCell(position: Position(x: 5, y: 5)))
+        playedCells.append(BattleCell(position: Position(x: 7, y: 7)))
+        
+        XCTAssertTrue(prizePosition.isAValidPrizePosition(on: playedCells))
+    }
+    
+    func test_prizeHasNoValidPosition() {
+        let prizePosition = Position(x: 5, y: 5)
+        var playedCells = [BattleCell]()
+        playedCells.append(BattleCell(position: Position(x: 1, y: 1)))
+        playedCells.append(BattleCell(position: Position(x: 5, y: 5)))
+        playedCells.append(BattleCell(position: Position(x: 7, y: 7)))
+        
+        XCTAssertFalse(prizePosition.isAValidPrizePosition(on: playedCells))
+    }
+    
+    func test_prizePositionIsOnBoard() {
+        let prizeRange = Position.Ranges.prize
+        let position = Position.generatePosition(for: prizeRange)
+        
+        XCTAssertNotNil(position)
+    }
 }
